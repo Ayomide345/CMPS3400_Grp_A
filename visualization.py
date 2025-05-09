11d969157d6d2b8ca0b2ac78257cec41857bb12d
Ayomide345 <157052297+Ayomide345@users.noreply.github.com>
2025-05-08 23:53:36 -0500

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -6,7 +6,7 @@ Version: v0.4
 Description:
     Plotting utilities for inventory data.
 Authors:
-    Taylor Fradella, Angel Njoku
+    Taylor Fradella, Angel Njoku, Ayomide Olubuse.
 Date Created     : 2025-04-12
 Date Last Updated: 2025-05-05
 
