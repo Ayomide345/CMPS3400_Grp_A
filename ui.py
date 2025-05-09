8c6e9f2ff86c220f9572f3c4eeb9917e496724f5
Ayomide345 <157052297+Ayomide345@users.noreply.github.com>
2025-05-08 23:53:00 -0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ Version: v0.3
 Description:
     CLI interface for Inventory Management.
 Authors:
-    Taylor Fradella, Angel Njoku
+    Taylor Fradella, Angel Njoku, Ayomide Olubuse.
 Date Created     : 2025-04-12
 Date Last Updated: 2025-05-XX
 
